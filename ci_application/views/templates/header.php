e2838c10f3c42d5ba8115aafb3b2dc1b23d5480b
Victor Ung <ungphoo4@gmail.com>
2013-04-24 12:57:23 -0700

    added scott's template files

--- ci_application/views/templates/header.php
+++ ci_application/views/templates/header.php
@@ -5,15 +5,23 @@
 	<?php 
 		/*Stylesheets*/
 		/*include url helper for this to work*/
+		echo base_url() . "css/index.css";
 	?>
 	<link href="<?= base_url() . "css/index.css" ?>" type="text/css" rel="stylesheet" />
 
 	<?php /*Scripts*/?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js" type="text/javascript"></script>
+	<script type = "text/javascript" src = "js/scripts.js"></script>
 </head>
 <body>
 	<header>
-		<h1><?= $pageTitle ?></h1>
+		<!--Search box-->
+		<input type="text" placeholder="Search for companies or products/services"/>
+		
+		<!--Profile/login/register-->
+		<a href = "login.php">
+			Me
+		</a>
 	</header>
 	
