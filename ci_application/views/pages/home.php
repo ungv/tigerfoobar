b1d91c8a8709315d5556777cf6acf755ee172549
Victor Ung <ungphoo4@gmail.com>
2013-05-07 22:48:05 -0700

    added evidence submission dialog

--- ci_application/views/pages/home.php
+++ ci_application/views/pages/home.php
@@ -8,20 +8,115 @@
 	$rows = $query->fetchAll(PDO::FETCH_ASSOC); */
 ?>
 
+<style type="text/css">
+ul {
+	float: left;
+	width: 960px;
+}
+
+li {
+	width: 150px;
+	height: 90px;
+	border: 1px
+	padding: 2px;
+	opacity: 0.8;
+	list-style-type: none;
+	float: left;
+}
+
+li:hover {
+	opacity: 1;
+}
+
+.rating {
+	float: left;
+	margin: 10px;
+}
+
+#submissions, #comments {
+	float: left;
+}
+
+#pasteURL {
+	width: 180px;
+	height: 20px;
+}
+
+#urlSubmit {
+	/* background: url('http://webhost.ischool.uw.edu/~bc28/submittingClaim.png');
+	background-repeat: no-repeat; */
+	background-color: #D1D2D4;
+	width: 780px;
+	height: 450px;
+	z-index: 100;
+	position: absolute;
+	margin-left: 70px;
+	padding: 10px;
+}
+
+#urlSubmit input, textarea {
+	margin-bottom: 5px;
+}
+
+#submit, #cancel {
+	float: right;
+}
+
+.full {
+	width: 100%;
+}
+
+.half {
+	width: 50%;
+}
+
+.quarter {
+	width: 25%;
+}
+</style>
+
 <script type="text/javascript">
+// Kudos Colors       -3         -2         -1         0          +1         +2         +3
+var kudosColors = ['#FF4900', '#FF7640', '#FF9B73', '#FEF5CA', '#61D7A4', '#36D792', '#00AF64'];
+
 $(document).ready(function() {
 	$.ajax({
 		type: 'GET',
-		url: 'http://webhost.ischool.uw.edu/~bc28/dbconnection.php',
+		url: 'http://webhost.ischool.uw.edu/~bc28/service.php',
 		data: {
-			sql: "SELECT * FROM Claim"
+			sql: "SELECT *, cl.Score AS ClaimScore, co.Score AS CoScore FROM Claim cl JOIN Company co ON cl.CompanyID = co.CompanyID"
 		},
 		contentType: 'jsonp',
 		dataType: 'jsonp',
 		success: function(json) {
-			
+			injectSubmissions(json);
 		},
 		error: ajaxError
+	});
+ 	$.each($('li'), function() {
+		$(this).attr('style', 'background-color:' + kudosColors[Math.ceil(Math.random() * 6)]);
+	});	
+});
+
+function injectSubmissions(json) {
+	$.each(json, function(i, claim) {
+		$a = $('<a>').attr('href', 'claim').text(claim.Title); // temporary link to claim template page
+		$li = $('<li>').attr('class', claim.ClaimScore).attr('style', 'background-color:' + kudosColors[parseInt(claim.ClaimScore) + 3]);
+		$li.append($a);
+		$('ul').prepend($li);
+	});
+}
+
+$(function() {
+	$("#pasteURL").bind('paste', function() {
+		$('#urlSubmit').show(200);
+		$(this).animate({"width": "100%",}, "fast" );
+	});
+	$("#cancel").click(function() {
+		$('#urlSubmit').hide(200);
+		$("#pasteURL").animate({"width": "180px",}, "fast" );
+		$("#pasteURL").val('');
+		return false;
 	});
 });
 
@@ -48,7 +143,33 @@ function ajaxError(jqxhr, type, error) {
 </script>
 
 <div id="main">
-	Front Page
+	<div id="urlContainer">
+		<input style="" id="pasteURL" placeholder="Paste URL to a new article"/>
+	</div>
+	
+	<div id="urlSubmit" style="display:none;">
+		<input class="full" type="text" placeholder="Title"/>
+		<textarea rows="4" cols="69%" placeholder="Your comments on this article"></textarea>
+		<input class="quarter" style="vertical-align: top;" type="text" placeholder="Associated Company"/>
+		<input class="full" type="text" placeholder="Tags"/>
+		<input type="submit" id="submit" value="Submit"/><input type="submit" id='cancel' value="cancel"  />
+	</div>
+	
 	<ul>
-	</ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>	
 </div>
--- ci_application/views/pages/profile.php
+++ ci_application/views/pages/profile.php
@@ -1,14 +1,20 @@
 <style type="text/css">
-td {
-	width: 100px;
-	height: 100px;
-	/* background-color should be applied through class set by its score 
-	background-color: rgb(111, 255, 249); */
-	padding: 2px;
-	opacity: 0.8;
+ul {
+	float: left;
+	width: 960px;
 }
 
-td:hover {
+li {
+	width: 150px;
+	height: 90px;
+	border: 1px
+	padding: 2px;
+	opacity: 0.8;
+	list-style-type: none;
+	float: left;
+}
+
+li:hover {
 	opacity: 1;
 }
 
@@ -16,37 +22,40 @@ td:hover {
 	float: left;
 	margin: 10px;
 }
+
+#submissions, #comments {
+	float: left;
+}
 </style>
 
 <script type="text/javascript">
+// Kudos Colors       -3         -2         -1         0          +1         +2         +3
 var kudosColors = ['#FF4900', '#FF7640', '#FF9B73', '#FEF5CA', '#61D7A4', '#36D792', '#00AF64'];
 $(document).ready(function() {
-	// Kudos Colors       -3         -2         -1         0          +1         +2         +3
 	$.ajax({
 		type: 'GET',
-		url: 'http://webhost.ischool.uw.edu/~bc28/dbconnection.php',
-		contentType: 'jsonp',
+		url: 'http://webhost.ischool.uw.edu/~bc28/service.php',
 		data: {
 			sql: "SELECT *, cl.Score AS ClaimScore, co.Score AS CoScore FROM Claim cl JOIN Company co ON cl.CompanyID = co.CompanyID"
 		},
+		contentType: 'jsonp',
 		dataType: 'jsonp',
 		success: function(json) {
 			injectSubmissions(json);
 		},
 		error: ajaxError
 	});
+	$.each($('li'), function() {
+		$(this).attr('style', 'background-color:' + kudosColors[Math.ceil(Math.random() * 6)]);
+	});
 });
 
 function injectSubmissions(json) {
 	$.each(json, function(i, claim) {
 		$a = $('<a>').attr('href', 'claim').text(claim.Title); // temporary link to claim template page
-		$td = $('<td>').attr('class', claim.ClaimScore).attr('style', 'background-color:' + kudosColors[parseInt(claim.ClaimScore) + 3]);
-		$td.append($a);
-		$('table').find('tr').append($td);
-	});
-	$.each($('td'), function() {
-		var thisRating = parseInt($(this).attr('class'));
-		$(this).attr('style', 'background-color:' + kudosColors[parseInt(claim.ClaimScore) + 3]);
+		$li = $('<li>').attr('class', claim.ClaimScore).attr('style', 'background-color:' + kudosColors[parseInt(claim.ClaimScore) + 3]);
+		$li.append($a);
+		$('ul').prepend($li);
 	});
 }
 
@@ -70,7 +79,6 @@ function ajaxError(jqxhr, type, error) {
 	}
 	alert(msg);
 }
-
 </script>
 
 <div id="main">
@@ -80,44 +88,20 @@ function ajaxError(jqxhr, type, error) {
 	
 	<div id="submissions">
 		<h3>Submissions:</h3>
-		<table>
-			<tr>
+		<ul>
 			<!-- inject boxes here -->
-			<!-- placeholder boxes to see what it looks like with more
-				<td class="1"><a href="claim">Apple plans to build $5 billion new headquarters in Cupertino</a></td>
-				<td class="-3"></td>
-				<td class="3"></td>
-				<td class="-1"></td>
-				<td class="-2"></td>
-				<td class="1"></td>
-				<td class="2"></td>
-				<td class="1"></td>
-				<td class="2"></td>
-			</tr>
-			<tr>
-				<td class="-1"></td>
-				<td class="3"></td>
-				<td class="2"></td>
-				<td class="-1"></td>
-				<td class="2"></td>
-				<td class="3"></td>
-				<td class="-1"></td>
-				<td class="-2"></td>
-				<td class="-1"></td>
-			</tr>
-			<tr>
-				<td class="1"></td>
-				<td class="1"></td>
-				<td class="3"></td>
-				<td class="-1"></td>
-				<td class="2"></td>
-				<td class="-2"></td>
-				<td class="2"></td>
-				<td class="1"></td>
-				<td class="1"></td>
-			-->
-			</tr>
-		</table>
+			<!-- placeholder boxes to see what it looks like with more -->
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+		</ul>
 	</div>
 	
 	<div id="comments">
