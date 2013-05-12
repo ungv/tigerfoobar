a8e6218adea9003d370019b47c2f09e48e5455ec
Brian <bc28@uw.edu>
2013-05-11 23:12:35 -0700

    work on the claim page

--- ci_application/views/pages/claim.php
+++ ci_application/views/pages/claim.php
@@ -1,28 +1,59 @@
 <div id="main">
-	<div id="title">
-		<h1>Apple plans to build $5 billion new headquarters in Cupertino</h1>
-		<div id="user">Submitted 2 months ago by <a href="profile">thisDude</a></div>
+	<h1>Apple plans to build $5 billion new headquarters in Cupertino</h1>
+
+	<div id="title" class="container">
+		<h2>Evidence -</h2>
+
+		<div id='evidenceContent'>
+			<dl>
+				<dt>URL: </dt>
+					<dd>link here</dd>
+				<dt>Synopsis: </dt>
+					<dd>Apple’s massive new spaceship-style headquarters is not on-budget or on schedule, according to a new report at Bloomberg...</dd>
+				<dt>Tags</dt>
+					<dd class='evidenceTags'>stevey jobbs</dd>
+					<dd class='evidenceTags'>stuff</dd>
+			</dl>
+			
+			<div id='infos'>
+				<p>By: Scotty Pippen</p>
+				<p>On May 11, 2013</p>
+			</div>
+		</div>
+
+		
+		
+		<!-- <div id="user">Submitted 2 months ago by <a href="profile">thisDude</a></div>
 		<div id="url">URL: <a href="http://www.google.com">Google</a></div>
 		<div id="synopsis">Synopsis: Apple’s massive new spaceship-style headquarters is not on-budget or on schedule, according to a new report at Bloomberg...</div>
 		<div id="tags">Tags:
 			<?php
 				// loop through tags table to display them in colored divs
 			?>
-		</div>
+		</div> -->
 	</div>
 	
 	<div class="container">
 		<h2>Score -</h2>
 		<div id="scoreContent" class="expanded">
+			<h3>+1.1 average <span>(100 opinions)</span></h3>
 			<section id='scoreMap'>
 				<!-- have to make form auto submit with js -->
 				<div id='scoreDistrubution'>
 <?php
-				for ($i=0; $i <=6 ; $i++) { 
+for ($i=0; $i <=6 ; $i++) { 
 ?>
-					<p style='top: <?=7	*$i?>px'></p>
+					<div style='top: <?=7	*$i?>px'>
 <?php
-				}
+for ($j=0; $j < 4; $j++) { 
+?>
+						<p class='circle'></p>
+<?php
+}
+?>	
+					</div>
+<?php
+}
 ?>	
 				</div>
 
