36b393088208fd0a59ef2186917bc4b63da4dd8f
Brian <bc28@uw.edu>
2013-05-18 20:30:41 -0700

    team

--- ci_application/views/pages/team.php
+++ ci_application/views/pages/team.php
@@ -2,8 +2,8 @@
 	<h1>Team Tiger Foobar</h1>
 	<ul id='team'>
 		<li>
-			<img src="img/sean.jpg">
-			<h2>Sean Johnstone</h2>
+			<img src="img/tiger.jpg">
+			<h2>Sean</h2>
 			<p>A guy that worked on this site.</p>
 			<p>Favorite Tiger: </p>
 			<p>Email: <a href="mailto:johnss5@uw.edu">johnss5@uw.edu</a></p>
@@ -12,8 +12,8 @@
 		<hr>
 		
 		<li>
-			<img src="img/brian.jpg">
-			<h2>Brian Chan</h2>
+			<img src="img/tiger.jpg">
+			<h2>Brian</h2>
 			<p>A guy that worked on this site.</p>
 			<p>Favorite Tiger: White Tiger</p>
 			<p>Email: <a href="mailto:bc28@uw.edu">bc28@uw.edu</a></p>
@@ -22,8 +22,8 @@
 		<hr>
 		
 		<li>
-			<img src="img/lauren.jpg">
-			<h2>Lauren Hanson</h2>
+			<img src="img/tiger.jpg">
+			<h2>Lauren</h2>
 			<p>A girl that worked on this site.</p>
 			<p>Favorite Tiger: </p>
 			<p>Email: <a href="mailto:lehanson@uw.edu">lehanson@uw.edu</a></p>
@@ -32,8 +32,8 @@
 		<hr>
 		
 		<li>
-			<img src="img/victor.jpg">
-			<h2>Victor Ung</h2>
+			<img src="img/tiger.jpg">
+			<h2>Victor</h2>
 			<p>A guy that worked on this site.</p>
 			<p>Favorite Tiger: </p>
 			<p>Email: <a href="mailto:ungv@uw.edu">ungv@uw.edu</a></p>
@@ -42,7 +42,7 @@
 		<hr>
 		
 		<li>
-			<img src="img/scott.jpg">
+			<img src="img/tiger.jpg">
 			<h2>Scott Kuehnert</h2>
 			<p>A guy that worked on this site.</p>
 			<p>Favorite Tiger: Snow Leopard</p>
