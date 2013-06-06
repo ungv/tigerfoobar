6a5969d77d1e62e6a142238343296f9a5c93fad7
Victor Ung <ungphoo4@gmail.com>
2013-06-05 22:13:17 -0700

    styled tags position

--- js/addClaim.js
+++ js/addClaim.js
@@ -64,7 +64,7 @@ function addClaim() {
 		$desc = $('textarea').val();
 		$company = $('#assocCo').tagit('tags');
 		$rating = $('input[name=score]:checked').val();
-		if ($rating == -3 || $rating == -2 || $rating == -1 || $rating == 1 || $rating == 2 || $rating == 3) {
+		if ($rating == null || $rating == -3 || $rating == -2 || $rating == -1 || $rating == 1 || $rating == 2 || $rating == 3) {
 		} else {
 			alert("Hey don't mess with us!");
 			window.location.reload(true);
